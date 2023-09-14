690a8cb55ed32d0371bd0afbf05c3ac895ffd0eb
iamrubydevi <ariesabyss@gmail.com>
2023-09-14 19:09:59 +0530

    Delete setup code

--- solver.rb
+++ solver.rb
@@ -1,11 +1,3 @@
 class Solver
-  def factorial(num)
-    if num.negative?
-      'Please enter a number greater than or equal to 0'
-    elsif [0, 1].include? num
-      1
-    else
-      num * factorial(num - 1)
-    end
-  end
+  # Write code here
 end
