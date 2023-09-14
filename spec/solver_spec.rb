e41aa301424965178462ce65c75c1aef40d32ae7
iamrubydevi <ariesabyss@gmail.com>
2023-09-14 18:42:28 +0530

    Write test for factorial method

---
+++ spec/solver_spec.rb
@@ -0,0 +1,26 @@
+require_relative '../solver'
+require 'rspec/mocks'
+
+describe Solver do
+  before(:each) do
+    @solver = Solver.new
+  end
+
+  context '#factorial method' do
+    it 'returns 1 for 0' do
+      expect(@solver.factorial(0)).to eq(1)
+    end
+
+    it 'returns 1 for 1' do
+      expect(@solver.factorial(1)).to eq(1)
+    end
+
+    it 'returns the correct factorial' do
+      expect(@solver.factorial(5)).to eq(120)
+    end
+
+    it 'raises an ArgumentError with negative integer' do
+      expect(@solver.factorial(-2)).to eq('Please enter a number greater than or equal to 0')
+    end
+  end
+end
