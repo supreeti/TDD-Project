a5bd5300bd903f5a10b08f5930c92ea4f02e96e7
Supreeti Kushwaha <sksupreetk11@gmail.com>
2023-09-15 16:19:19 +0530

    add solver.rb for spec file

--- solver.rb
+++ solver.rb
@@ -1,3 +1,27 @@
 class Solver
-  # Write code here
+  def factorial(number)
+    if number.negative?
+      'Please enter a number greater than or equal to 0'
+    elsif [0, 1].include?(number)
+      1
+    else
+      number * factorial(number - 1)
+    end
+  end
+
+  def reverse(str)
+    str.reverse
+  end
+
+  def fizzbuzz(num)
+    if (num % 3).zero? && (num % 5).zero?
+      'fizzbuzz'
+    elsif (num % 3).zero?
+      'fizz'
+    elsif (num % 5).zero?
+      'buzz'
+    else
+      num.to_s
+    end
+  end
 end
